dffe3210fb55d2ffc8c3b8b80777726fc8c2dbfc
Nikhil Pandey <91266675+Nikhil70700@users.noreply.github.com>
2025-08-15 00:33:27 +0530

    Played with colors and Bg Based On Conditions

--- Android Development/Android Dev Projects/BMI/app/src/main/java/com/example/bmi/MainActivity.java
+++ Android Development/Android Dev Projects/BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -22,13 +23,23 @@ public class MainActivity extends AppCompatActivity {
 
         EditText editWeight,editHeightInc,editHeightFt;
         Button btnCalculate;
-        TextView txtResult;
+        TextView txtResult,WTxt,HFtTxt,HInTxt;
+        LinearLayout llMain;
+
+
 
         editWeight=findViewById(R.id.edtWeight);
         editHeightFt=findViewById(R.id.edtHeightFt);
         editHeightInc=findViewById(R.id.edtHeightInc);
         btnCalculate=findViewById(R.id.btnCalculate);
         txtResult=findViewById(R.id.tvResult);
+        llMain=findViewById(R.id.llMain);
+        WTxt=findViewById(R.id.WTxt);
+        HFtTxt=findViewById(R.id.HFtTxt);
+        HInTxt=findViewById(R.id.HInTxt);
+
+
+
 
         btnCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,10 +55,21 @@ public class MainActivity extends AppCompatActivity {
 
                 if(bmi>25){
                     txtResult.setText(R.string.OverWeight);
+                    WTxt.setTextColor(getResources().getColor(R.color.white));
+                    HFtTxt.setTextColor(getResources().getColor(R.color.white));
+                    HInTxt.setTextColor(getResources().getColor(R.color.white));
+                    txtResult.setTextColor(getResources().getColor(R.color.white));
+                    llMain.setBackgroundColor(getResources().getColor(R.color.over_weight));
                 }else if(bmi<18){
                     txtResult.setText(R.string.UnderWeight);
+                    WTxt.setTextColor(getResources().getColor(R.color.black));
+                    HFtTxt.setTextColor(getResources().getColor(R.color.black));
+                    HInTxt.setTextColor(getResources().getColor(R.color.black));
+                    txtResult.setTextColor(getResources().getColor(R.color.black));
+                    llMain.setBackgroundColor(getResources().getColor(R.color.underweight));
                 }else{
                     txtResult.setText(R.string.healthy);
+                    llMain.setBackgroundColor(getResources().getColor(R.color.healthy));
                 }
             }
         });
